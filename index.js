1c06f045d9302994d9c44d957ec235f560d12c36
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-07-10 22:05:38 +0000

    This is my first work on javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+// let or var is used to declare variables (but let is most prefared)
+// working with string leterals
+let firstName = "Emmanuel";
+let lastName = "Agbosu";
+console.log(firstName, lastName);
+
+// working with number leterals
+let fisrtNumber = 20;
+let secondNumber = 30;
+console.log(fisrtNumber + secondNumber);
+
+// working with booleans 
+let isMale = true;
+let isFemale = false;
+console.log(isMale);
+
+// uninitialized varriables in js are set as undifined 
+// the null is used to clear data stored in a varriable
+firstName = null;
+console.log(firstName); // we cam see that the first name is now empty
